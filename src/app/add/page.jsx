0e6dee75edee0312ea38e2d68ec0636dea3fafc9
Shayan-Mst <shnbackup007@gmail.com>
2025-06-14 13:58:04 +0330

    added button

--- src/app/add/page.jsx
+++ src/app/add/page.jsx
@@ -1,10 +1,13 @@
+import Button from '@/components/Button';
 import React from 'react'
 
  const Add = () => {
   return (
     <form className='absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 max-w-md w-full'>
+      <h2 className='text-white text-center my-4 text-[32px]'>ADD TODO</h2>
       <input type='text' placeholder='enter title of work' className='bg-white text-black p-2 my-2 rounded-lg w-full'/>
       <textarea type="text" placeholder="Description (optional)" className="bg-white text-black p-2 my-2 rounded-lg w-full"></textarea>
+      <Button color='green' children={"Add todo"}/>
     </form>
   )
 }
