4fd3f2bd110b377f8dce340693e570b004901564
Shayan-Mst <shnbackup007@gmail.com>
2025-06-14 13:53:38 +0330

    created form and inputs of add page

--- src/app/add/page.jsx
+++ src/app/add/page.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
  const Add = () => {
   return (
-    <div>add</div>
+    <form className='absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 max-w-md w-full'>
+      <input type='text' placeholder='enter title of work' className='bg-white text-black p-2 my-2 rounded-lg w-full'/>
+      <textarea type="text" placeholder="Description (optional)" className="bg-white text-black p-2 my-2 rounded-lg w-full"></textarea>
+    </form>
   )
 }
 
